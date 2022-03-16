69aff093faedb19d91627e86fc4551553069cda5
Paul_Ratchata <paul_ratchata@PaulM1.local>
2022-03-16 10:26:43 +0700

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,7 +22,12 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     HttpClientModule,
     CoreModule,
-    SharedModule
+    SharedModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideMessaging(() => getMessaging()),
+    provideStorage(() => getStorage())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'mj-it-services',
+    appId: '1:470852601787:web:c3f09bf6873d753070a69e',
+    databaseURL: 'https://mj-it-services-default-rtdb.asia-southeast1.firebasedatabase.app',
+    storageBucket: 'mj-it-services.appspot.com',
+    locationId: 'asia-southeast1',
+    apiKey: 'AIzaSyBR2TUJ_Dde8OtiJrwausnrq7F872cVWwg',
+    authDomain: 'mj-it-services.firebaseapp.com',
+    messagingSenderId: '470852601787',
+    measurementId: 'G-47EDS3X9QN',
+  },
   production: false
 };
 
